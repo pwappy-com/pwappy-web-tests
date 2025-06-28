b911b5807ffcb6b9db0d430ed083d8468b47b1c5
pwappy-com <pwappy.com@gmail.com>
2025-06-29 01:47:38 +0900

    macだけ動かないのでデバッグ

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,7 +38,8 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    //trace: 'on-first-retry',
+    trace: 'on',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
   },
