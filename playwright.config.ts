95e818da633b5d06ce0f132f9db20c0364c6ff27
pwappy-com <pwappy.com@gmail.com>
2026-01-16 01:56:37 +0900

    リトライ回数の調整

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
 
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 5 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? undefined : undefined,
   /** タイムアウト設定
