4bea329126a007abf38594d16492bc91d5b698e7
pwappy-com <pwappy.com@gmail.com>
2026-04-06 22:27:25 +0900

    テスト安定化

--- playwright.config.ts
+++ playwright.config.ts
@@ -47,7 +47,8 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
+    //trace: 'retain-on-failure',
+    trace: 'off',
     //trace: 'on',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
